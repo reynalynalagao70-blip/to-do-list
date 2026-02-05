b6c9b18ab76ba176add67595e9f8bb550893e23c
Lablab ( ˘ ³˘)♥︎ <reynalynalagao70@gmail.com>
2026-02-05 09:56:20 +0800

    server

--- server/db.js
+++ server/db.js
@@ -1,14 +1,25 @@
-import pkg from "pg";
-import dotenv from "dotenv";
-
+import { Pool } from "pg";
+import dotenv from "dotenv"
 dotenv.config();
 
-const { Pool } = pkg;
+let newPool;
 
-export const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 5432,
-});
+if (process.env.NODE_ENV === 'development') {
+    newPool = new Pool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT,
+    });
+}
+else {
+    newPool = new Pool({
+        ssl: {
+            rejectUnauthorized: false,
+        },
+        connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:5432/${process.env.DB_NAME}?options=project=${process.env.ENDPOINT_ID}`,
+    });
+}
+
+export const pool = newPool;
